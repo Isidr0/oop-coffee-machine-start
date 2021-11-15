c39174e30afc0f991a51998b591f2278f8f2965f
Isidr0 <rtwatson86@gmail.com>
2021-11-15 15:49:24 -0500

    working solution

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
+# snake case means all lower case separated by underscores.
 my_menu = Menu()
 my_coffee_maker = CoffeeMaker()
 my_money_machine = MoneyMachine()
